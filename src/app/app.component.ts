626fb134272f4f83c8db12cc2b5e216aee9704e3
Muzammil Ansari <muzammil.ansari@tacto.in>
2020-02-08 13:05:10 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import Amplify , {Auth} from 'aws-amplify';
+import awsconfig from '../aws-exports';
+Amplify.configure(awsconfig);
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'CognitoV5';
+  isLogin:Boolean = false;
+  constructor() {
+    var that = this;
+    Auth.currentSession()
+    .then(data => {
+      that.isLogin = true;
+    })
+    .catch(err => {
+      that.isLogin = false;
+    });
+
+    Auth.currentCredentials()
+    .then(data => {
+      console.log("boom 21",data)
+    })
+    .catch(err => console.log(err));
+  }
+
+  signin(){
+    Auth.federatedSignIn();
+  }
+
+  signout(){
+    Auth.signOut()
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  }
 }
